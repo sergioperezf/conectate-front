270dbfbbce2adb24cedbf0e08d69991657d583ee
Wendell Arias <wdarias@unicesar.edu.co>
2018-04-12 19:52:54 -0500

    corregir bug add herramienta

--- src/app/add-tool/add-tool.component.ts
+++ src/app/add-tool/add-tool.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-tool',
+  providers : [ToolService],
   templateUrl: './add-tool.component.html',
   styleUrls: ['./add-tool.component.css']
 })

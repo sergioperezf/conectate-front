193a1901cdffcabc90d1ae2bd47e486c63299117
Unknown <vivimendoza16@gmail.com>
2018-04-18 22:52:49 -0500

    Pruebas Funcionales v1

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, ProtractorExpectedConditions } from 'protractor';
 
 describe('conectate App', () => {
   let page: AppPage;
@@ -7,8 +8,46 @@ describe('conectate App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  /*it('should display Conectate Page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Conectate');
   });
+
+  it('should display Menu Options', () => {
+    page.navigateTo();
+    page.getMenu();
+    expect(page.getOptions()).toEqual(['Inicio', 'Agregar herramienta']);
+  });
+
+  it('should Make Valid Search', () => {
+    var EC=ProtractorExpectedConditions;
+    page.navigateTo();
+    page.enterValidKeyWords('Moodle');
+    expect(page.getSearch()).toEqual('Conectate');
+  });
+
+  it('should navigate to add tool page', () => {
+    page.navigateTo();
+    page.getMenu();
+    expect(page.navigateToAddToolPage()).toEqual('Agregar herramienta');
+  });
+
+  it('should navigate to add example page', () => {
+    page.navigateTo();
+    expect(page.navigateToAddExamplePage()).toEqual('Herramienta x');
+  });
+
+  it('should navigate to tool detail', () => {
+    page.navigateToDetail(1);
+    expect(page.navigateToDetail(1)).toEqual('Moodlle');
+  });*/
+
+  it('should edit tool draft', () => {
+    page.navigateTo();
+    page.getMenu();
+    page.navigateToAddToolPage();
+    page.editToolDraft('Tool1','Description','Empty');
+    expect(page.navigateToAddToolPage()).toEqual('Agregar herramienta');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,63 @@ export class AppPage {
     return browser.get('/');
   }
 
+  navigateToDetail(id: number) {
+    browser.get('/tool/' + id + '/detail');
+    return element(by.css('.row h4')).getText();
+
+  }
+
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getMenu() {
+    return element(by.css('.mat-icon-button')).click()
+  }
+
+  getOptions() {
+    return element.all(by.css('app-root a')).getText();
+  }
+
+  enterValidKeyWords(search: string) {
+    element(by.tagName('input')).sendKeys(search);
+    browser.sleep(2000);
+    element(by.buttonText('Buscar')).click();
+    browser.sleep(2000);
+
+  }
+
+  getSearch() {
+    //return element(by.tagName('input')).getText();
+    return element(by.css('app-root h1')).getText();
+  }
+
+  navigateToAddToolPage() {
+    element(by.linkText('Agregar herramienta')).click();
+    browser.sleep(2000);
+    return element(by.css('app-root h2')).getText();
+  }
+
+  navigateBackToHome() {
+    element(by.linkText('Inicio')).click();
+    browser.sleep(2000);
+    return element(by.css('app-root h1')).getText();
+  }
+
+  navigateToAddExamplePage() {
+    element(by.css('.fab.fa-searchengin')).click();
+    browser.sleep(2000);
+    return element(by.css('.col-md-12 h1')).getText();
+  }
+
+  editToolDraft(ToolName: string, Desc: string, Restrict: string) {
+    element(by.id('txtNombre')).sendKeys(ToolName);
+    browser.sleep(2000);
+    element(by.tagName('textarea')).id('mat-input-29').sendKeys(Desc);
+    browser.sleep(2000);
+    element(by.id('mat-input-30')).sendKeys(Restrict);
+    browser.sleep(2000);
+    return element(by.css('app-root h2')).getText();
+  }
+
 }

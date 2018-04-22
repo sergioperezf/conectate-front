aaebbd24f545ba679c31678e9aed1ed69b000382
Unknown <vivimendoza16@gmail.com>
2018-04-22 01:55:24 -0500

    Inicio feature 25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AddToolComponent} from "./add-tool/add-tool.component";
 import { DetailToolComponent} from "./detail-tool/detail-tool.component";
 import { ListTutorialComponent } from './list-tutorial/list-tutorial.component';
 import {ApproveToolComponent} from './approve-tool/approve-tool.component';
+import { RequestReviewComponent } from './request-review/request-review.component';
+
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'detalles/home', component: HomeComponent },
@@ -14,6 +16,7 @@ const routes: Routes = [
   { path: 'tool', component:AddToolComponent},
   { path: 'tool/lista', component: HomeComponent},
   { path: 'tool/aprobar', component: ApproveToolComponent},
+  { path: 'review', component: RequestReviewComponent},
   { path: 'tool/:id/detail', component: DetailToolComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
---
+++ src/app/request-review/request-review.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Tool} from '../models/tool.models';
+import {ToolService} from '../services/tool.service';
+
+@Component({
+  selector: 'app-request-review',
+  providers: [ToolService],
+  templateUrl: './request-review.component.html',
+  styleUrls: ['./request-review.component.css']
+})
+export class RequestReviewComponent implements OnInit {
+
+  private tool: Tool;
+  private title: string = "Detalle de herramienta";
+  private admin: Boolean = false;
+
+  private loadingPublish: Boolean = true;
+  private errorMessage: Boolean = true;
+
+  private idTool: number;
+
+  constructor(private route: ActivatedRoute, private toolService: ToolService, public router: Router) {
+    this.tool = new Tool();
+  }
+
+
+  ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.idTool = params['id'];
+    });
+
+    this.getDetail();
+
+    let sesion = sessionStorage.getItem('login');
+    if (sesion) {
+      this.admin = true;
+    }
+    
+  }
+
+    getDetail() {
+    this.toolService.get(this.idTool).subscribe(
+      result => {
+        this.tool = result;        
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  publishTool() {
+    this.loadingPublish = true;
+    let stateAndId = {
+      "state": "Publicado",
+      "id": this.idTool,
+      "name":this.tool.name,
+      "description": this.tool.description
+    };
+    console.log(stateAndId);
+    this.toolService.publish(stateAndId).subscribe(() => {
+      this.router.navigate(['tool/lista']);
+      this.loadingPublish = false;
+    }, (err) => {
+      console.log(err);
+      this.errorMessage = false;
+    });
+  }
+
+}
